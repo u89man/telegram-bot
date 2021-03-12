2e8b19e1e3a97b0602cffdda0cb6a2618380cbfd
u89man <u89man@gmail.com>
2021-03-12 22:53:59 +0500

    Добавляет класс сущности "MessageAutoDeleteTimerChanged".

---
+++ src/Entities/MessageAutoDeleteTimerChanged.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace U89Man\TBot\Entities;
+
+/**
+ * @link https://core.telegram.org/bots/api#messageautodeletetimerchanged
+ *
+ * @method int getMessageAutoDeleteTime()
+ */
+class MessageAutoDeleteTimerChanged extends Entity
+{
+    //
+}
