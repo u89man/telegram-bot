d38ec114dcd139104113d68e4902f3b1c0aa628e
u89man <u89man@gmail.com>
2021-03-12 22:21:28 +0500

    Добавляет класс сущности "VoiceChatEnded".

---
+++ src/Entities/VoiceChatEnded.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace U89Man\TBot\Entities;
+
+/**
+ * @link https://core.telegram.org/bots/api#voicechatended
+ *
+ * @method int getDuration()
+ */
+class VoiceChatEnded extends Entity
+{
+    //
+}
