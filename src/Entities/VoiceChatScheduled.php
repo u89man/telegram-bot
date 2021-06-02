833b210a91a647dfad4776b8bdaa3b64514e5d4b
u89man <u89man@gmail.com>
2021-06-02 19:18:51 +0500

    Добавляет сущность "VoiceChatScheduled" для представления служебного сообщения о запланированом голосовом чате.

---
+++ src/Entities/VoiceChatScheduled.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace U89Man\TBot\Entities;
+
+/**
+ * @link https://core.telegram.org/bots/api#voicechatscheduled
+ *
+ * @method int getStartDate()
+ */
+class VoiceChatScheduled extends Entity
+{
+    //
+}
