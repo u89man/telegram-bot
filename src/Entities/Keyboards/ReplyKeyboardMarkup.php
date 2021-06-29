b041163c4c0792f0c6c39fa203c42b64762e175b
u89man <u89man@gmail.com>
2021-06-29 21:37:32 +0500

    Добавляет методы и параметры для работы с полем "input_field_placeholder" в сущности ReplyKeyboardMarkup.

--- src/Entities/Keyboards/ReplyKeyboardMarkup.php
+++ src/Entities/Keyboards/ReplyKeyboardMarkup.php
@@ -5,14 +5,16 @@ namespace U89Man\TBot\Entities\Keyboards;
 /**
  * @link https://core.telegram.org/bots/api#replykeyboardmarkup
  *
- * @method bool|null getResizeKeyboard()
- * @method bool|null getOneTimeKeyboard()
- * @method bool|null getSelective()
+ * @method   bool|null getResizeKeyboard()
+ * @method   bool|null getOneTimeKeyboard()
+ * @method string|null getInputFieldPlaceholder()
+ * @method   bool|null getSelective()
  *
- * @method     $this setKeyboard(array $keyboard)
- * @method     $this setResizeKeyboard(bool $resizeKeyboard)
- * @method     $this setOneTimeKeyboard(bool $oneTimeKeyboard)
- * @method     $this setSelective(bool $selective)
+ * @method       $this setKeyboard(array $keyboard)
+ * @method       $this setResizeKeyboard(bool $resizeKeyboard)
+ * @method       $this setOneTimeKeyboard(bool $oneTimeKeyboard)
+ * @method       $this setInputFieldPlaceholder(string $inputFieldPlaceholder)
+ * @method       $this setSelective(bool $selective)
  */
 class ReplyKeyboardMarkup extends Keyboard
 {
@@ -28,6 +30,7 @@ class ReplyKeyboardMarkup extends Keyboard
 	 * @param KeyboardButton[][]|string[][] $keyboard
 	 * @param bool|null $resizeKeyboard
 	 * @param bool|null $oneTimeKeyboard
+	 * @param string|null $inputFieldPlaceholder
 	 * @param bool|null $selective
      *
 	 * @return ReplyKeyboardMarkup
@@ -36,12 +39,14 @@ class ReplyKeyboardMarkup extends Keyboard
 		$keyboard,
 		$resizeKeyboard = null,
 		$oneTimeKeyboard = null,
+		$inputFieldPlaceholder = null,
 		$selective = null
-	){
+	) {
 	    return new static([
 			'keyboard' => $keyboard,
 			'resize_keyboard' => $resizeKeyboard,
 			'one_time_keyboard' => $oneTimeKeyboard,
+			'input_field_placeholder' => $inputFieldPlaceholder,
 			'selective' => $selective,
 		]);
 	}
