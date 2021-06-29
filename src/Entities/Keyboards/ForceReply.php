8a62538ced6d84ec68863740f62f0a36353f4b03
u89man <u89man@gmail.com>
2021-06-29 21:43:53 +0500

    Добавляет методы и параметры для работы с полем "input_field_placeholder" в сущности ForceReply.

--- src/Entities/Keyboards/ForceReply.php
+++ src/Entities/Keyboards/ForceReply.php
@@ -5,23 +5,29 @@ namespace U89Man\TBot\Entities\Keyboards;
 /**
  * @link https://core.telegram.org/bots/api#forcereply
  *
- * @method      bool getForceReply()
- * @method bool|null getSelective()
- * @method     $this setForceReply(bool $forceReply)
- * @method     $this setSelective(bool $selective)
+ * @method        bool getForceReply()
+ * @method string|null getInputFieldPlaceholder()
+ * @method   bool|null getSelective()
+ *
+ * @method       $this setForceReply(bool $forceReply)
+ * @method       $this setInputFieldPlaceholder(string $inputFieldPlaceholder)
+ * @method       $this setSelective(bool $selective)
  */
 class ForceReply extends Keyboard
 {
 	/**
 	 * @param bool|null $selective
+     * @param string|null $inputFieldPlaceholder
      *
 	 * @return ForceReply
 	 */
 	public static function make(
+        $inputFieldPlaceholder = null,
 	    $selective = null
     ) {
 	    return new static([
 			'force_reply' => true,
+            'input_field_placeholder' => $inputFieldPlaceholder,
 			'selective' => $selective
 		]);
 	}
