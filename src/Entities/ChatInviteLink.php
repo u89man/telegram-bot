796cd26c937dc63fd13087e221ca09e25d22b29d
u89man <u89man@gmail.com>
2022-05-23 18:16:52 +0500

    Добавляет метод "getName()" в сущность "ChatInviteLink".

--- src/Entities/ChatInviteLink.php
+++ src/Entities/ChatInviteLink.php
@@ -5,14 +5,15 @@ namespace U89Man\TBot\Entities;
 /**
  * @link https://core.telegram.org/bots/api#chatinvitelink
  *
- * @method   string getInviteLink()
- * @method     User getCreator()
- * @method     bool getCreatesJoinRequest()
- * @method     bool getIsPrimary()
- * @method     bool getIsRevoked()
- * @method int|null getExpireDate()
- * @method int|null getMemberLimit()
- * @method int|null getPendingJoinRequestCount()
+ * @method      string getInviteLink()
+ * @method        User getCreator()
+ * @method        bool getCreatesJoinRequest()
+ * @method        bool getIsPrimary()
+ * @method        bool getIsRevoked()
+ * @method string|null getName()
+ * @method    int|null getExpireDate()
+ * @method    int|null getMemberLimit()
+ * @method    int|null getPendingJoinRequestCount()
  */
 class ChatInviteLink extends Entity
 {
