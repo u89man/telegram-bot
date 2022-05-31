1bfe0a6c01e08c031a62a9e360e7ab5e7071c208
u89man <u89man@gmail.com>
2022-05-31 21:53:11 +0500

    Добавляет сущность "WebAppInfo".

---
+++ src/Entities/WebAppInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace U89Man\TBot\Entities;
+
+/**
+ * @link https://core.telegram.org/bots/api#webappinfo
+ *
+ * @method string getUrl()
+ */
+class WebAppInfo extends Entity
+{
+    //
+}
