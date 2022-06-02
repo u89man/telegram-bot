6c530ca67360efbe236713e7cd974b05fed3b6bd
u89man <u89man@gmail.com>
2022-06-02 22:48:02 +0500

    Добавляет класс сущности "WebAppData".

---
+++ src/Entities/WebAppData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace U89Man\TBot\Entities;
+
+/**
+ * @link https://core.telegram.org/bots/api#webappdata
+ *
+ * @method string getData()
+ * @method string getButtonText()
+ */
+class WebAppData extends Entity
+{
+    //
+}
