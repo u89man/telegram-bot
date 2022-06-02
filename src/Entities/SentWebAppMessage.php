b6371bebe8975bccd709140357cadd14a167163d
u89man <u89man@gmail.com>
2022-06-02 22:35:32 +0500

    Добавляет класс сущности "SentWebAppMessage".

---
+++ src/Entities/SentWebAppMessage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace U89Man\TBot\Entities;
+
+/**
+ * @link https://core.telegram.org/bots/api#sentwebappmessage
+ *
+ * @method string getInlineMessageId()
+ */
+class SentWebAppMessage extends Entity
+{
+    //
+}
