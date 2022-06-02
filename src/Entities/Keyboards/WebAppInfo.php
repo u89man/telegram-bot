bb845778c28824c87f303fdbf2b4d77e261c19fd
u89man <u89man@gmail.com>
2022-06-02 22:52:00 +0500

    Добавляет метод "setUrl()" сущности "WebAppInfo".

--- src/Entities/Keyboards/WebAppInfo.php
+++ src/Entities/Keyboards/WebAppInfo.php
@@ -2,14 +2,14 @@
 
 namespace U89Man\TBot\Entities\Keyboards;
 
-use U89Man\TBot\Entities\Entity;
-
 /**
  * @link https://core.telegram.org/bots/api#webappinfo
  *
  * @method string getUrl()
+ *
+ * @method  $this setUrl(string $url)
  */
-class WebAppInfo extends Entity
+class WebAppInfo extends Keyboard
 {
     /**
      * @param string $url
